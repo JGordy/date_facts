3ca4d9ee03cbb0d8b6b3b6b1056cd3a4d72c1ef0
Joseph Gordy <jgordy2424@gmail.com>
2017-08-14 15:37:10 -0400

    All answers are now complete using moment and colored using chalk

---
+++ index.js
@@ -0,0 +1,23 @@
+const moment = require("moment");
+const chalk = require("chalk");
+
+let now = moment();
+console.log(moment);
+
+console.log("It is " + chalk.blue(moment().format("dddd, MMMM Do YYYY, h:mm:ss a")) );
+
+console.log("It is the " + chalk.magenta(moment().format("DDDo")) + " day of the year.");
+
+console.log("It is " + chalk.cyan(moment().hours() * 60 * 60 + moment().minutes() * 60 + moment().seconds()) + " seconds into the day.");
+
+if (moment().isDST() == true) {
+  console.log("It " + chalk.green("is ") + "during daylight savings time.");
+} else {
+  console.log("It" + chalk.green("is not ") + "during daylight savings time.");
+}
+
+if (moment().isLeapYear() == true) {
+  console.log("It " + chalk.red("is ") + "a leap year");
+} else {
+  console.log("It " + chalk.red("is not ") + "a leap year.");
+}
